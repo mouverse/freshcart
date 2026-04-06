1c5699a7147a015ed8f95782507ef668d36416ae
Moustafa Aly <moustafa.aly7010@gmail.com>
2026-04-06 21:43:23 +0200

    fix: adjust toast position

--- src/components/providers/ToastProvider.tsx
+++ src/components/providers/ToastProvider.tsx
@@ -5,7 +5,7 @@ import { Toaster } from 'sonner';
 export default function ToastProvider() {
   return (
     <Toaster
-      position="top-right"
+      position="bottom-right"
       richColors
       closeButton
       duration={4000}
